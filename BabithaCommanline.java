af1a6de40b4662504173a3dba3d5be700399cd82
sheri-babitha <99714139+sheri-babitha@users.noreply.github.com>
2022-09-28 20:40:43 +0530

    Add files via upload

---
+++ BabithaCommanline.java
@@ -0,0 +1,9 @@
+class BabithaCommanline{
+public static void main(String args[]){ 
+int count= args.length;
+System.out.println("no of command line argument is " + count);
+ for (int i=0;i<= count;i++)
+{System.out.println(args[i]);}
+}
+}
+
